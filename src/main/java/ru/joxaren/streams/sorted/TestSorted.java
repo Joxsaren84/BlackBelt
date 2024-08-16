2eca47646404e8d5f02ba332ccee92b877308551
Keypa <42-18@mail.ru>
2024-08-16 12:18:49 +1000

    add stream sorted examples

---
+++ src/main/java/ru/joxaren/streams/sorted/TestSorted.java
@@ -0,0 +1,35 @@
+package ru.joxaren.streams.sorted;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TestSorted {
+
+    public static void main(String[] args) {
+
+        int [] array = {256, 2, 8, 32, 16, 4, 128, 64};
+        array = Arrays.stream(array).sorted().toArray();
+        System.out.println(Arrays.toString(array));
+
+        List<Student> list = new ArrayList<>();
+
+        Student st1 = new Student("Alex", 'm', 23, 4, 4.5);
+        Student st2 = new Student("Gerta", 'f', 19, 1, 5);
+        Student st3 = new Student("George", 'm', 21, 2, 3.9);
+        Student st4 = new Student("Junior", 'm', 20, 1, 4);
+        Student st5 = new Student("Lola", 'f', 22, 3, 4.1);
+
+        list.add(st1);
+        list.add(st2);
+        list.add(st3);
+        list.add(st4);
+        list.add(st5);
+
+        list = list.stream().sorted((s1, s2) -> s1.getName().compareTo(s2.getName())).collect(Collectors.toList());
+        System.out.println(list);
+
+    }
+
+}
