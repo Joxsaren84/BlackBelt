d6fae593ecae69e50f84b4efb80f8c912e997800
Keypa <42-18@mail.ru>
2024-08-23 12:01:25 +1000

    add stream methods parallelStream examples

---
+++ src/main/java/ru/joxaren/streams/parallelstream/TestParallelStream.java
@@ -0,0 +1,29 @@
+package ru.joxaren.streams.parallelstream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestParallelStream {
+
+    public static void main(String[] args) {
+
+        List<Double> list = new ArrayList<>();
+
+        list.add(10.0);
+        list.add(5d);
+        list.add(1d);
+        list.add(0.25);
+
+
+        Double sum = list.parallelStream().reduce((a, e) -> a + e).get();
+        System.out.println(sum);
+
+        Double dif = list.stream().reduce((a, e) -> a / e).get();
+        System.out.println(dif);
+
+        Double difP = list.parallelStream().reduce((a, e) -> a / e).get();
+        System.out.println(dif);
+
+    }
+
+}
